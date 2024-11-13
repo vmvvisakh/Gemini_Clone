0d53c9ed955e8ca7d48c62672b7fde247a1f8500
vmvvisakh <visakhvijayan798@@gmail.com>
2024-11-13 16:49:54 +0530

    Initial commit

---
+++ src/config/gemini.js
@@ -0,0 +1,37 @@
+
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai"
+  
+  const apiKey = 'AIzaSyD_GtwEKFv9B3UV-UUWyf5kOAzvNwUpRzQ';
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return response.text()
+  }
+  
+  export default run
---
+++ src/context/Context.jsx
@@ -0,0 +1,51 @@
+import { createContext, useState } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+
+const ContextProvider = (props) =>{
+
+    const [input,setInput] = useState('')
+    const [recentPrompt,setRecentPrompt] = useState('')
+    const [prevPrompts,setPrevPrompts] = useState([])
+    const [showResult,setShowResult]=useState(false)
+    const [loading,setLoading] = useState(false)
+    const [resultData,setResultData] = useState("")
+
+    const onSent = async (prompt) =>{
+        setResultData("")
+        setShowResult(true)
+        setLoading(true)
+        
+       const response = await run(input)
+       setResultData(response)
+       setLoading(false)
+       setInput("")
+    }
+
+    
+
+    const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+
+}
+
+
+export default ContextProvider
